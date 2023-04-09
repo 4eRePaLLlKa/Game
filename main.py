25229f07d77f29e7a869d4c02cfc923f5126bed4
4eRePaLLlKa <109215860+4eRePaLLlKa@users.noreply.github.com>
2023-04-09 18:32:09 +0300

    add btn

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
+
 from pygame import *
+from button import Button
+
 
 window = display.set_mode((700,500))
 
 game = True
 clock = time.Clock()
 
+
+btn1 = Button('start.png',100,100,100,50)
+btn2 = Button('exit.png',200,200,200,50)
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
+    window.fill((255,255,255))
 
-    window.fill((222,11,22))
+    if btn1.draw(window):
+        print("Press")
+    btn2.draw(window)
+
 
     display.update()
     clock.tick(60)
+
+
