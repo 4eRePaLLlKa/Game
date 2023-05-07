9b761118936e601ca4e974e395defb1f859225c9
4eRePaLLlKa <109215860+4eRePaLLlKa@users.noreply.github.com>
2023-05-07 18:55:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 from pygame import *
 from button import Button
-from sprite import Player, GameSprite, Wall, Bullet
+from sprite import Player, GameSprite, Wall, Bullet, Enemy
+from random import randint
 
 
 window = display.set_mode((700,500))
@@ -9,7 +10,9 @@ window = display.set_mode((700,500))
 background_image = image.load("arena.png")
 background = transform.scale(background_image, (700,500))
 
-
+font.init()
+font1 = font.SysFont("Arial", 20)
+img = font1.render("Score: ",True,(255,255,255))
 
 game = True
 clock = time.Clock()
@@ -29,6 +32,15 @@ wall4 = Wall(200,35, 110,375, transperancy=0)
 
 bullets = sprite.Group()
 bullets = []
+enemys = []
+
+for i in range(2):
+    enemy1 = Enemy("zombie1.png",randint(640,650),20,30,45,randint(1,2))
+    enemy2 = Enemy("zombie2.png",randint(640,650),320,30,45,randint(1,2))
+    enemy3 = Enemy("zombie3.png",randint(640,650),420,30,45,randint(1,2))
+    enemys.append(enemy1)
+    enemys.append(enemy2)
+    enemys.append(enemy3)
 
 
 while game:
@@ -48,18 +60,24 @@ while game:
         player.draw(window)
         player.move()
 
-
+        window.blit(img, (20,450))
         wall1.draw(window)
         wall2.draw(window)
         wall3.draw(window)
         wall4.draw(window)
         
 
-        for b in bullets :
+        for b in bullets:
             b.update(window)
+
+        for e in enemys:
+            e.update(window)
+
+
 
     else:
         window.fill((0,0,0))
+        
         if btn1.draw(window):
             run = True
             print("Press")
--- sprite.py
+++ sprite.py
@@ -1,4 +1,5 @@
 from pygame import *
+from random import randint
 
 class GameSprite(sprite.Sprite):
     def __init__(self,image_name,x,y,width,height,speed):
@@ -14,6 +15,7 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
+    self.img_1 = transform.scale(image.load("bullet.png"), (10,15))
     def move(self):
         keys = key.get_pressed()
         if keys[K_UP] == True and self.rect.y != 20:
@@ -21,26 +23,37 @@ class Player(GameSprite):
         elif keys[K_DOWN] == True and self.rect.y != 410:
             self.rect.y = self.rect.y + self.speed 
 
-    def fire(self,Bullet):
-        bullet = Bullet(self.rect.centerx, self.rect.centery)
-        return bullets
+def fire(self):
+        if self.image == self.img_1:
+            bullet = Bullet(self.rect.centerx, self.rect.centery)
+        return bullet
 
 
 class Bullet(sprite.Sprite):
-    def __init__(self,x,y):
-        self.bull = Surface(20,20)
+    def __init__(self, x,y):
+        self.bull = Surface((20,20))
+        self.bull.fill((255,0,0))
         self.rect = self.bull.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.speed = 5 
+        self.speed = 5
 
-
-    def update(self,window):
-        window.blit(self.bull,(self.rect.x,self.rect.y))
+    def update(self, window):
+        window.blit(self.bull, (self.rect.x, self.rect.y))
 
 class Enemy(GameSprite):
-    def move(self):
-        pass
+    def update(self,window):
+        self.rect.x = self.rect.x - self.speed
+        points = [15,120,220,320,420]
+        global lost
+        if self.rect.x < 200:
+            self.rect.x = randint(640,650)        
+            self.rect.y = points[randint(0,4)]
+            self.speed = randint(1,2)
+        window.blit(self.image,(self.rect.x,self.rect.y))
+
+
+
 
 class Wall():
     def __init__(self, width,height, x,y, color=(255,255,255), transperancy=255):
